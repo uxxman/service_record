b3a9474d693843dd1ed6ff3eb135bbb8e8d1abb4
Muhammad Usman <uxman.sherwani@gmail.com>
2022-01-28 23:16:56 +0500

    Adding code comments (#9)

--- lib/service_record/base.rb
+++ lib/service_record/base.rb
@@ -5,6 +5,11 @@ require 'service_record/failure'
 require 'service_record/callbacks'
 
 module ServiceRecord
+  # Base class to be extended by all service classes
+  #
+  #   class MyService < ServiceRecord
+  #   end
+  #
   class Base
     include Callbacks
     include ActiveModel::Attributes
@@ -13,6 +18,8 @@ module ServiceRecord
 
     attr_accessor :result
 
+    # Wrapper around the *perform* instance method that runs all the validations
+    # and callbacks before eventually calling *perform*.
     def self.perform(args = {})
       new.tap do |service|
         service.attributes = args
@@ -24,6 +31,7 @@ module ServiceRecord
       end
     end
 
+    # Wapper around the *perform* class method that raises exception if service fails
     def self.perform!(args = {})
       service = perform(args)
       return service if service.success?
@@ -31,14 +39,17 @@ module ServiceRecord
       raise Failure, service
     end
 
+    # Checks the service for errors. Returns +true+ if no errors are found, +false+ otherwise.
     def success?
       errors.empty?
     end
 
+    # Checks the service for errors. Returns +false+ if no errors are found, +true+ otherwise.
     def failure?
       !success?
     end
 
+    # Each subclass must define the *perform* method
     def perform
       raise NotImplementedError
     end

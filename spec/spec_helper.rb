25a3781941033b33c03688024b821abb59f6abcf
Usman <uxman.sherwani@gmail.com>
2023-07-15 03:33:45 +0200

    Manually require simplecov_json_formatter for code-climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'simplecov_json_formatter'
+
+SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
 SimpleCov.start
 
 require 'service_record'
